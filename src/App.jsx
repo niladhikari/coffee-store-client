8c2d6e209a9c264b8d1ef983bb332a5f7368788c
niladhikari <niladhikari445@gmail.com>
2023-10-13 21:28:14 +0600

    add get the data in the server and load the data.and display the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 
 
+import { useLoaderData } from 'react-router-dom'
 import './App.css'
+import CoffeeCard from './Components/CoffeeCard'
 
 function App() {
+
+  const coffees = useLoaderData()
 
 
   return (
     <>
       
-      <h1 className='text-green-500'>Vite + React</h1>
+      <h1 className='text-green-500 text-5xl font-bold'>Hot Hot Cold Coffee : {coffees.length}</h1>
+      <div className='grid md:grid-cols-2 gap-6 mt-20'>
+        {
+          coffees.map(coffee=> <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+        }
+      </div>
     
     </>
   )
