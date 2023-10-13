ae8aaeda0b635337eca01db09759f25aaebe8a6e
niladhikari <niladhikari445@gmail.com>
2023-10-13 19:13:58 +0600

    add the some route

---
+++ src/Components/CoffeeCard.jsx
@@ -0,0 +1,11 @@
+
+
+const CoffeeCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CoffeeCard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App';
+import AddCoffee from './Components/AddCoffee';
+import UpdateCoffee from './Components/UpdateCoffee';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/addCoffee",
+    element: <AddCoffee></AddCoffee>,
+  },
+  {
+    path: "/updateCoffee",
+    element: <UpdateCoffee></UpdateCoffee>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
