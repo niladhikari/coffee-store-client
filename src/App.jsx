6189de27a17a69d680ec88de68987c22b84d94e5
niladhikari <niladhikari445@gmail.com>
2023-10-14 12:12:37 +0600

    read the data using post and read the data useing get method

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@
 import { useLoaderData } from 'react-router-dom'
 import './App.css'
 import CoffeeCard from './Components/CoffeeCard'
+import { useState } from 'react';
 
 function App() {
-
-  const coffees = useLoaderData()
+  
+  const loadedCoffees  = useLoaderData()
+  const [coffees, setCoffees] = useState(loadedCoffees);
 
 
   return (
@@ -15,7 +17,13 @@ function App() {
       <h1 className='text-green-500 text-5xl font-bold'>Hot Hot Cold Coffee : {coffees.length}</h1>
       <div className='grid md:grid-cols-2 gap-6 mt-20'>
         {
-          coffees.map(coffee=> <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+          coffees.map(coffee=> <CoffeeCard 
+            key={coffee._id}
+            coffee={coffee}
+            coffees={coffees}
+            setCoffees={setCoffees}
+
+          ></CoffeeCard>)
         }
       </div>
     
